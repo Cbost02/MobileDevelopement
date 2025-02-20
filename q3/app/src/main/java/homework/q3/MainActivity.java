31f974bc9684e549c904bcf68982071a7ae3f53f
Cromwell Bost <cbost@emich.edu>
2025-02-20 16:12:30 -0500

    empty app

--- q3/app/src/main/java/homework/q3/MainActivity.java
+++ q3/app/src/main/java/homework/q3/MainActivity.java
@@ -22,37 +22,4 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
-    public void calculate(View v)
-    {
-        EditText input_one_box = findViewById(R.id.principal);
-        EditText input_two_box = findViewById(R.id.a_addition);
-        EditText input_three_box = findViewById(R.id.years);
-        EditText input_four_box = findViewById(R.id.investment);
-
-
-        String principal_string = input_one_box.getText().toString();
-        String annual_addition_string = input_two_box.getText().toString();
-        String number_of_years_string = input_three_box.getText().toString();
-        String investment_rate_string = input_four_box.getText().toString();
-
-
-        float principal = Float.parseFloat(principal_string);
-        float annual_addition = Float.parseFloat(annual_addition_string);
-        float number_of_years = Float.parseFloat(number_of_years_string);
-        float investment_rate = Float.parseFloat(investment_rate_string);
-
-        Calculator calculator = new Calculator(principal, annual_addition, investment_rate, number_of_years);
-
-        int total = calculator.getAmount();
-
-
-        String total_string = "$";
-        total_string += total;
-
-        TextView final_amount_box = findViewById(R.id.output);
-
-        final_amount_box.setText(total_string);
-    }
 }

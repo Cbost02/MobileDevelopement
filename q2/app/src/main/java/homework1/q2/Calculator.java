b7f1f3bd14274418c8a36de8c0bd04ab4acb0573
Cromwell Bost <cbost@emich.edu>
2025-02-25 14:37:34 -0500

    App is around 75% complete; need to complete program functionality; testing needed.

---
+++ q2/app/src/main/java/homework1/q2/Calculator.java
@@ -0,0 +1,17 @@
+package homework1.q2;
+
+public class Calculator
+{
+    private float price;
+    private boolean warranty;
+    private boolean insurance;
+    private String shipping;
+
+    public Calculator(float price, boolean warranty, boolean insurance, String shipping)
+    {
+        this.price = price;
+        this.warranty = warranty;
+        this.insurance = insurance;
+        this.shipping = shipping;
+    }
+}
--- q2/app/src/main/java/homework1/q2/MainActivity.java
+++ q2/app/src/main/java/homework1/q2/MainActivity.java
@@ -2,8 +2,12 @@ package homework1.q2;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +21,53 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        ButtonHandler handler = new ButtonHandler(); // Create Button Handler
+        Button button = findViewById(R.id.calculate_button); // Call the button from the layout
+        button.setOnClickListener(handler); // Attach the handler to the button
         setContentView(R.layout.activity_main);
     }
+
+    private class ButtonHandler implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View v)
+        {
+            // Calling and reading the information from the Price widget
+            EditText price_box = findViewById(R.id.price_input);
+            String price_string = price_box.getText().toString();
+            float price = Float.parseFloat(price_string);
+
+            // Calling and reading the information from the Price widget
+            ToggleButton warranty_toggle = findViewById(R.id.warranty_button);
+            boolean warranty = false; // default value
+
+            // Check to see if the toggle button is checked, if so, set warranty to 'true
+            if(warranty_toggle.isChecked())
+            {
+                warranty = true;
+            }
+
+            // Calling and reading the information from the Insurance widget
+            Switch insurance_switch = findViewById(R.id.insurance_switch);
+            boolean insurance = false; // default value
+
+            // Check to see if the switch is checked, if so, set insurance to 'true
+            if(insurance_switch.isChecked())
+            {
+                insurance = true;
+            }
+
+            // Calling and reading the information from the Shipping widget
+            Spinner shipping_spinner = findViewById(R.id.delivery_input);
+            String shipping = ""; // default value
+
+            // Make a list of all the spinner attributes
+
+
+
+
+
+        }
+    }
+
 }

b4bad21315f77ef40686246ff8e11995f185a631
Cromwell Bost <cbost@emich.edu>
2025-02-26 04:00:36 -0500

    App is complete! Make sure to go back & redo/add comments

--- q7/app/src/main/java/homework/q7/Game.java
+++ q7/app/src/main/java/homework/q7/Game.java
@@ -1,6 +1,89 @@
 package homework.q7;
 
-public class Game
-{
+public class Game {
+    private char[][] current_board;
+    private char[][] goal_board;
+    private int x; // Row of the empty slot
+    private int y; // Column of the empty slot
 
+    public Game() {
+        // Creating a generator & getting the initial board
+        Generator generator = new Generator();
+        current_board = generator.generateInitialBoard();
+        goal_board = generator.generateGoalBoard();
+        findEmptySlot(); // Find the initial position of the empty slot, *before* any moves
+
+    }
+
+    private void findEmptySlot() {
+        for (int i = 0; i < current_board.length; i++) {
+            for (int j = 0; j < current_board[i].length; j++) {
+                if (current_board[i][j] == ' ') { // Assuming ' ' represents the empty slot
+                    x = i;
+                    y = j;
+                    return; // Exit the loops once found
+                }
+            }
+        }
+    }
+    public void up() {
+        if (x > 0) {
+            // No need to find the empty slot again; x and y are already tracking it
+            char temp = current_board[x][y]; // Use a temporary variable for swapping
+            current_board[x][y] = current_board[x - 1][y];
+            current_board[x - 1][y] = temp;
+            x--; // Update x after the swap
+        }
+    }
+
+    public void down() {
+        if (x < current_board.length - 1) {
+            char temp = current_board[x][y];
+            current_board[x][y] = current_board[x + 1][y];
+            current_board[x + 1][y] = temp;
+            x++;
+        }
+    }
+
+    public void left() {
+        if (y > 0) {
+            char temp = current_board[x][y];
+            current_board[x][y] = current_board[x][y - 1];
+            current_board[x][y - 1] = temp;
+            y--;
+        }
+    }
+
+    public void right() {
+        if (y < current_board[0].length - 1) {
+            char temp = current_board[x][y];
+            current_board[x][y] = current_board[x][y + 1];
+            current_board[x][y + 1] = temp;
+            y++;
+        }
+    }
+
+
+    public char[][] getCurrent_board() {
+        return current_board;
+    }
+
+    public char[][] getGoal_board() {
+        return goal_board;
+    }
+
+    public boolean isSolved()
+    {
+        for (int i = 0; i < current_board.length; i++)
+        {
+            for (int j = 0; j < current_board[i].length; j++)
+            {
+                if (current_board[i][j] != goal_board[i][j])
+                {
+                    return false; // If any element doesn't match, it's not solved
+                }
+            }
+        }
+        return true; // If all elements match, it's solved
+    }
 }
--- q7/app/src/main/java/homework/q7/MainActivity.java
+++ q7/app/src/main/java/homework/q7/MainActivity.java
@@ -2,6 +2,8 @@ package homework.q7;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,22 +19,57 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        game = new Game();
-        ButtonHandler handler = new ButtonHandler();
-        screen = new AppInterface(this,handler);
         super.onCreate(savedInstanceState);
+        game = new Game(); // initialing the game
+        ButtonHandler handler = new ButtonHandler(); // creating a button handler
+        screen = new AppInterface(this,handler); // initializing the interface
+        setContentView(screen);
 
+        // get the current board & display it
+        screen.drawCurrent(game.getCurrent_board());
+
+        // get the goal board & display it
+        screen.drawGoal(game.getGoal_board());
     }
+
+
+
 
     private class ButtonHandler implements View.OnClickListener
     {
         @Override
         public void onClick(View v)
         {
-            // Find the button that was clicked
+            char buttonId = screen.findButton((Button) v);
+            switch (buttonId)
+            {
+                case '1': // Up
+                    game.up();
+                    break;
+                case '2': // Down
+                    game.down();
+                    break;
+                case '3': // Left
+                    game.left();
+                    break;
+                case '4': // Right
+                    game.right();
+                    break;
+            }
+            screen.drawCurrent(game.getCurrent_board()); // Update the display
 
+            if(game.isSolved())
+            {
+                String message = "You won the game!";
+                int duration = Toast.LENGTH_LONG;
+                Toast toast = Toast.makeText(MainActivity.this, message, duration);
+                toast.show();
+                screen.disableButtons();
 
-            // Display current board
+            }
         }
+
+
+
     }
 }
